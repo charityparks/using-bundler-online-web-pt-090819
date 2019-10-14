42abde9ef865d5493e6c50398030f49b915ab2ed
Charity Parks <tuppercharity@gmail.com>
2019-10-14 20:04:27 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,4 @@
-require_relative '../config/environment'
+#require_relative '../config/environment'
 
 Hashie
 Sinatra
@@ -6,4 +6,4 @@ Octokit
 Pry
 AwesomePrint
 
-
+require_relative '../config/environment'
